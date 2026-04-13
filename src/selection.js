d3552c912f98b00d950a2b2ca8094101d88c1cbf
Foley <teriomon5@aol.com>
2026-04-13 01:14:35 -0400

    end of day

--- src/selection.js
+++ src/selection.js
@@ -6,11 +6,42 @@
 // Also drag and drop ships should be a thing, however that means changing the harbor up to have enough space for each ship.
 // Maybe can change vert horz to flipping 90degs each time for more customization of boards.
 
-const orientBtns = document.querySelectorAll('.orient');
+// const orientBtns = document.querySelectorAll('.orient');
 
-orientBtns.forEach((btn) => {
-  const shipClass = '.' + btn.id.replace('-orient', '');
-  const ship = document.querySelector(shipClass);
+// orientBtns.forEach((btn) => {
+//   const shipClass = '.' + btn.id.replace('-orient', '');
+//   const ship = document.querySelector(shipClass);
+//   btn.addEventListener('click', (e) => {
+//     btn.classList.toggle('horz');
+//     btn.classList.toggle('vert');
+//     if (btn.classList.contains('horz')) {
+//       btn.textContent = 'Horz';
+//     } else {
+//       btn.textContent = 'Vert';
+//     }
+//     ship.classList.toggle('flipped');
+//   });
+// });
+
+const labels = document.querySelectorAll('.square-label');
+for (let i = 0; i < 20; i++) {
+  const label = labels[i];
+  console.log(label);
+  if (i < 10) {
+    label.style.setProperty('grid-column', `${i + 2}`);
+    label.style.setProperty('grid-row', `1`);
+  } else {
+    label.style.setProperty('grid-column', '1');
+    label.style.setProperty('grid-row', `${i + 2 - 10}`);
+  }
+}
+
+const shipContainers = document.querySelectorAll('.ship-container');
+shipContainers.forEach((cont) => {
+  const ship = cont.querySelector('img');
+  const btn = cont.querySelector('button');
+  const xInput = cont.querySelector('x-input');
+  const yInput = cont.querySelector('y-input');
   btn.addEventListener('click', (e) => {
     btn.classList.toggle('horz');
     btn.classList.toggle('vert');
